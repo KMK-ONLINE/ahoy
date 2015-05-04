5937bf9c46ed8fa1af28f3cd04675712dad7cf57
Yap Sok Ann <sokann@gmail.com>
2015-05-04 11:01:13 +0700

    Generate UUID from `Math.random` + current timestamp to avoid collisions
    See: http://stackoverflow.com/questions/6906916/collisions-when-generating-uuids-in-javascript
    
    We get thousands of collisions a day with the old solution. To see if this
    works first, otherwise will try node-uuid that makes use of browser's
    crypto api.

--- vendor/assets/javascripts/ahoy.js
+++ vendor/assets/javascripts/ahoy.js
@@ -83,12 +83,15 @@
     }
   }
 
-  // http://stackoverflow.com/a/2117523/1177228
+  // http://stackoverflow.com/a/8809472
   function generateId() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-      return v.toString(16);
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
     });
+    return uuid;
   }
 
   function saveEventQueue() {
